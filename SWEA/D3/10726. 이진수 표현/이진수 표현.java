a1fb97d58d65183c26f82784705cb299edc81387
thdus <107402806+thdus@users.noreply.github.com>
2025-05-08 23:54:10 +0900

    [D3] Title: 이진수 표현, Time: 240 ms, Memory: 46,844 KB -BaekjoonHub

---
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+	
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		int first = (1<<N)-1;
+		boolean second = (M&first)==first;
+		
+		System.out.println("#"+test_case+" "+ (second ? "ON":"OFF"));
+
+		}sc.close();
+	}
+}
