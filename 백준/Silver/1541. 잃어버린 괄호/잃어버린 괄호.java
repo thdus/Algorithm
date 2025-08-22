dfd0936ebb2d3cea3caca0de9ebf1f83a2c5b517
thdus <107402806+thdus@users.noreply.github.com>
2025-08-22 15:19:51 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 64 ms, Memory: 11676 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,24 +1,25 @@
 import java.io.*;
-import java.util.*;
 
-public class Main {
-	public static void main(String[] args) throws IOException {
+public class Main{
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String s = br.readLine();
 		String[] arr = s.split("-");
-		int[] arr1 = new int[arr.length];
-		for (int i = 0; i < arr.length; i++) {
-			String[] temp = arr[i].split("\\+");
-			for (int j = 0; j < temp.length; j++) {
-				arr1[i] += Integer.parseInt(temp[j]);
-			}
-		}
-
-		int result = arr1[0];
-		for (int i = 1; i < arr1.length; i++) {
-			result -= arr1[i];
-		}
 		
+		int[] sum = new int[arr.length];
+		
+		for(int i=0; i<arr.length; i++) {
+			String[] arr2 = arr[i].split("[+]");
+			
+			for(int j=0; j<arr2.length; j++) {
+				sum[i]+=Integer.parseInt(arr2[j]);
+			}
+			
+		}
+		int result = sum[0];
+		for(int i=1; i<arr.length; i++) {
+			result -= sum[i];
+		}
 		System.out.println(result);
 	}
-}
+} 
