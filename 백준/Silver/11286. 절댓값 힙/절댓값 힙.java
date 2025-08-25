fe3a6e4cea957902442f9f4767bb0bbf05f00fef
thdus <107402806+thdus@users.noreply.github.com>
2025-08-26 01:49:09 +0900

    [Silver I] Title: 절댓값 힙, Time: 260 ms, Memory: 24764 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,55 +1,48 @@
 
 import java.io.*;
-
 import java.util.*;
 
-public class Main {
+public class Main{
 	public static void main(String[] args) throws IOException{
-		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> maxH = new PriorityQueue<>();
+		StringBuilder sb = new StringBuilder();
 		PriorityQueue<Integer> minH = new PriorityQueue<>(Collections.reverseOrder());
-		
+		PriorityQueue<Integer> maxH = new PriorityQueue<>();
+		int N = Integer.parseInt(br.readLine());
 		for(int i=0; i<N; i++) {
 			int x = Integer.parseInt(br.readLine());
 			if(x==0) {
-				int min = 0;
-				int max =0;
-				
-				 if (!minH.isEmpty())
-	                    min = minH.remove();
-	                if (!maxH.isEmpty())
-	                    max = maxH.remove();
-				
-				if(min==0&&max!=0) {
-					sb.append(max).append('\n');
+				int min=0;
+				int max=0;
+				if(!minH.isEmpty()) {
+					min = minH.remove();
 				}
-				else if(min==0&&max==0) {
+				if(!maxH.isEmpty()) {
+					max = maxH.remove();
+				}
+				
+				if(min==0&&max==0) {
 					sb.append(0).append('\n');
-				}
-				else if(min!=0&&max==0) {
+				}else if(min==0&&max!=0){
+					sb.append(max).append('\n');
+				}else if(min!=0&&max==0) {
 					sb.append(min).append('\n');
-				}
-				else {
-					if(min*-1>max) {
-						sb.append(max).append('\n');
-						minH.add(min);
-					}else {
+				}else {
+					if(min*-1<=max) {
 						sb.append(min).append('\n');
 						maxH.add(max);
+					}else {
+						sb.append(max).append('\n');
+						minH.add(min);
 					}
 				}
-
+			}
+			else if(x<0) {
+				minH.add(x);
 			}else {
-				if(x<0) {
-					minH.add(x);
-				}else {
-					maxH.add(x);
-				}
+				maxH.add(x);
 			}
 		}
 		System.out.println(sb);
-		
 	}
 }
