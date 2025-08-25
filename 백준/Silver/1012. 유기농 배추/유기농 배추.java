8e1ecdcb056ef7d46760fba0382edc2e9f0b516b
thdus <107402806+thdus@users.noreply.github.com>
2025-08-26 00:47:23 +0900

    [Silver II] Title: 유기농 배추, Time: 96 ms, Memory: 13612 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,23 +1,22 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main{
 	static int[] dx = {-1,1,0,0};
 	static int[] dy = {0,0,-1,1};
+	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		
-		while(T-- >0) {
+		while(T-->0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int m = Integer.parseInt(st.nextToken());
-			int n = Integer.parseInt(st.nextToken());
-			int k = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			int N = Integer.parseInt(st.nextToken());
+			int K = Integer.parseInt(st.nextToken());
 			
-			int[][] arr = new int[n][m];
-			for(int i=0; i<k; i++) {
+			int[][] arr = new int[N][M];
+			for(int i=0; i<K; i++) {
 				st = new StringTokenizer(br.readLine());
 				int x = Integer.parseInt(st.nextToken());
 				int y = Integer.parseInt(st.nextToken());
@@ -25,9 +24,9 @@ public class Main{
 			}
 			int cnt=0;
 			Queue<int[]> q = new LinkedList<>();
-			boolean visited[][]=new boolean[n][m];
-			for(int y=0; y<n; y++) {
-				for(int x=0; x<m; x++) {
+			boolean[][] visited = new boolean[N][M];
+			for(int y=0; y<N; y++) {
+				for(int x=0; x<M; x++) {
 					if(arr[y][x]==1&&!visited[y][x]) {
 						cnt++;
 						int[] a = {x,y};
@@ -38,16 +37,17 @@ public class Main{
 								int nx = e[0]+dx[d];
 								int ny = e[1]+dy[d];
 								
-								if(nx<0||ny<0||nx>=m||ny>=n||arr[ny][nx]==0||visited[ny][nx]) {
-									continue;
-								}
-								q.add(new int[]{nx,ny});
-								visited[ny][nx]=true;							}
+								if(nx<0||nx>=M||ny<0||ny>=N||arr[ny][nx]==0||visited[ny][nx]) continue;
+								q.add(new int[] {nx,ny});
+								visited[ny][nx]=true;
+								
+							}
 						}
 					}
 				}
 			}
-			sb.append(cnt).append('\n');
+			sb.append(cnt).append('\n');			
+		
 		}
 		System.out.println(sb);
 	}
