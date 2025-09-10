348a329204eb05ea202fdf59b8ef2e2c45ca31f1
thdus <107402806+thdus@users.noreply.github.com>
2025-09-10 23:08:13 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 256 ms, Memory: 36844 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -1,59 +1,65 @@
+
+
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    static int dx[] = { -1, 1, 0, 0 };
-    static int dy[] = { 0, 0, -1, 1 };
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        int IX = -1; 
-        int IY = -1; 
-
-        char arr[][] = new char[N][M];
-        for (int y = 0; y < N; y++) {
-            arr[y] = br.readLine().toCharArray(); 
-            for (int x = 0; x < M; x++) {
-                if (arr[y][x] == 'I') { 
-                    IY = y;
-                    IX = x;
-                }
-            }
-        }
-
-        boolean visited[][] = new boolean[N][M]; 
-
-        Queue<int[]> q = new LinkedList<>();
-        
-        q.add(new int[] { IX, IY }); 
-        visited[IY][IX] = true;
-
-       
-        int cnt = 0;
-
-        while (!q.isEmpty()) {
-            int now[] = q.remove();
-           
-            for (int d = 0; d < 4; d++) {
-                int nx = now[0] + dx[d];
-                int ny = now[1] + dy[d];
-                if (nx < 0 || ny < 0 || nx >= M || ny >= N) 
-                    continue;
-                if (arr[ny][nx] == 'X') 
-                    continue;
-                if (visited[ny][nx]) 
-                    continue;
-                if (arr[ny][nx] == 'P') 
-                    cnt++;
-                
-                visited[ny][nx] = true; 
-                q.add(new int[] { nx, ny });
-            }
-        }
-        System.out.println(cnt == 0 ? "TT" : cnt); 
-    }
+public class Main{
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int Ii = -1;
+		int Ij = -1;
+		
+		char[][] arr = new char[N][M];
+		for(int i=0; i<N; i++) {
+			arr[i] = br.readLine().toCharArray();
+			for(int j=0; j<M; j++) {
+				if(arr[i][j]=='I') {
+					Ii = i;
+					Ij = j;
+				}
+			}
+		}
+		
+		boolean[][] visited = new boolean[N][M];
+		
+		Queue<int[]> q = new LinkedList<>();
+		
+		q.add(new int[] {Ii,Ij});
+		
+		visited[Ii][Ij] = true;
+		
+		int cnt=0;
+		
+		while(!q.isEmpty()) {
+			int[] now = q.remove();
+			
+			for(int i=0; i<4; i++) {
+				int nx = now[0]+dx[i];
+				int ny = now[1]+dy[i];
+				
+				if(nx<0||ny<0||nx>=N||ny>=M) {
+					continue;
+				}
+				
+				if(visited[nx][ny]) continue;
+				if(arr[nx][ny]=='X') continue;
+				if(arr[nx][ny]=='P') cnt++;
+				
+				visited[nx][ny] = true;
+				q.add(new int[] {nx,ny});
+			}
+		}
+		
+		if(cnt==0) {
+			System.out.println("TT");
+		}else {
+			System.out.println(cnt);
+		}
+	}
 }
