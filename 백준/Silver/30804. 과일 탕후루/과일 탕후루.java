36cf1e598a25e0793fb377d8a94f471b07f72fed
thdus <107402806+thdus@users.noreply.github.com>
2025-09-11 23:15:33 +0900

    [Silver II] Title: 과일 탕후루, Time: 272 ms, Memory: 28152 KB -BaekjoonHub

---
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        int max = 0;
+    
+        for (int num1 = 1; num1 <= 8; num1++) {
+            for (int num2 = num1 + 1; num2 <= 9; num2++) {
+                int cnt = 0;
+  
+                for (int i = 0; i < N; i++) {
+               
+                    if (arr[i] == num1 || arr[i] == num2)
+                        cnt++; 
+                    else {
+                        
+                        if (cnt > max)
+                            max = cnt;
+                      
+                        cnt = 0;
+                    }
+                }
+               
+                if (cnt > max)
+                    max = cnt;
+            }
+        }
+        System.out.println(max);
+    }
+}
