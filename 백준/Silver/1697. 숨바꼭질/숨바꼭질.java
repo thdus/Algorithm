cd25a9485d99099966a4d92c97a411591260b18f
thdus <107402806+thdus@users.noreply.github.com>
2025-09-23 01:20:27 +0900

    [Silver I] Title: 숨바꼭질, Time: 108 ms, Memory: 17980 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,49 +1,47 @@
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final int MAX = 100000;
-
+  
+static int Max = 100000;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); 
-        int K = Integer.parseInt(st.nextToken());
-
-      
-        if (N == K) {
-            System.out.println(0);
-        }
-
-     
-        boolean visited[] = new boolean[MAX + 1];
-
-      
-        Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[] { 0, N }); 
-        visited[N] = true; 
-
-        while (!q.isEmpty()) {
-            int now[] = q.remove();
-            int time = now[0];
-            int x = now[1];
-
-            
-            int next[] = { x - 1, x + 1, x * 2 };
-
-            for (int d : next) {
-                if (d < 0 || d > MAX) 
-                    continue;
-                if (visited[d])
-                    continue;
-                if (d == K) { 
-                    System.out.println(time + 1);
-                }
-
-               
-                visited[d] = true;
-                q.offer(new int[] { time + 1, d });
-            }
-        }
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+    	int N = Integer.parseInt(st.nextToken());
+    	int K = Integer.parseInt(st.nextToken());
+    	
+    	if(N==K) {System.out.println(0);
+    	return;}
+    	
+    	boolean[] visited = new boolean[Max+1];
+    	Queue<int[]> q = new ArrayDeque<>();
+    	
+    	q.offer(new int[] {0,N});
+    	visited[N] = true;
+    	
+    	while(!q.isEmpty()) {
+    		int[] now = q.remove();
+    		int time = now[0];
+    		int x = now[1];
+    		
+    		int[] next = {x-1, x+1, x*2};
+    		
+    		for(int d : next) {
+    			if(d<0||d>Max) continue;
+    			if(visited[d]) continue;
+    			if(d==K) {
+    				System.out.println(time+1);
+    				return;
+    			}
+    			
+    			visited[d] = true;
+    			q.offer(new int[] {time+1,d});
+    			
+    		}
+    	}
+    	
+    	
     }
 }
