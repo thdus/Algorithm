91b14843c3b81f807d71891aac48ba883574f2c2
thdus <107402806+thdus@users.noreply.github.com>
2025-10-20 16:01:04 +0900

    [Silver IV] Title: 동전 0, Time: 108 ms, Memory: 12236 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,28 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int k = Integer.parseInt(input[1]);
-        
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int sum = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            while (k >= arr[i]) {        
-                k -= arr[i];             
-                sum++;                   
-            }
-        }
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
 
-        System.out.println(sum);
-    }
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		int cnt = 0;
+
+		for (int i = n - 1; i >= 0; i--) {
+			if (arr[i] <= k) {
+				while (arr[i] <= k) {
+					k = k - arr[i];
+					cnt++;
+				}
+			}
+		}
+		System.out.println(cnt);
+	}
 }
