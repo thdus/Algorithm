43e9870385b54f8a543c7e6d7bcf696e5854bf9c
thdus <107402806+thdus@users.noreply.github.com>
2025-10-21 20:57:45 +0900

    [Silver III] Title: 계단 오르기, Time: 64 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,28 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+import java.util.*;
+import java.io.*;
 
-        int n = Integer.parseInt(br.readLine());
-        int[] stair = new int[n + 1]; 
-        int[] dp = new int[n + 1];    
-
-        for (int i = 1; i <= n; i++) {
-            stair[i] = Integer.parseInt(br.readLine());
-        }
-
-        dp[1] = stair[1];
-        if (n >= 2) {
-            dp[2] = stair[1] + stair[2];
-        }
-
-        for (int i = 3; i <= n; i++) {
-            dp[i] = Math.max(dp[i - 2], dp[i - 3] + stair[i - 1]) + stair[i];
-        }
-
-        System.out.println(dp[n]);
-    }
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n+1];
+		int[] dp = new int[n+1];
+		
+		for(int i=1; i<=n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		dp[1] = arr[1];
+		if(n>=2) {
+			dp[2] = arr[1]+arr[2];
+		}
+		
+		for(int i=3; i<=n; i++) {
+			dp[i] = arr[i]+Math.max(dp[i-2], arr[i-1]+dp[i-3]);
+		}
+		
+		System.out.println(dp[n]);
+		
+	}
 }
