8fb23ab4b8cd72cc1d6ea0aafb104a04e01ea9a7
thdus <107402806+thdus@users.noreply.github.com>
2025-10-24 15:16:08 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 72 ms, Memory: 11828 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,41 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine()); 
-
-        for (int test = 0; test < t; test++) {
-            int n = Integer.parseInt(br.readLine());
-
-            HashMap<String, Integer> map = new HashMap<>();
-
-            for (int i = 0; i < n; i++) {
-                String line = br.readLine();
-                String[] parts = line.split(" ");
-                String type = parts[1];
-
-                
-                if (!map.containsKey(type)) {
-                    map.put(type, 1);
-                } else {
-                    int value = map.get(type);
-                    map.put(type, value + 1);
-                }
-            }
-
-            int answer = 1;
-            for (String key : map.keySet()) {
-                int count = map.get(key);
-                answer *= (count + 1);
-            }
-
-            answer -= 1; 
-            System.out.println(answer);
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		while(t-->0) {
+			int n = Integer.parseInt(br.readLine());
+			Map<String, Integer> hm = new HashMap();
+			for(int i=0; i<n; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				String a = st.nextToken();
+				String b = st.nextToken();
+				
+				hm.put(b,hm.getOrDefault(b, 0)+1);
+			}
+			int ans = 1;
+			for(int num : hm.values()) {
+				ans*=num+1;
+			}
+			
+			sb.append(ans-1+"\n");
+			
+		}
+		System.out.println(sb);
+		
+	}
 }
