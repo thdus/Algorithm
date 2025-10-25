de331d62bbc6663fe4ba06e4864840dbb94bba03
thdus <107402806+thdus@users.noreply.github.com>
2025-10-25 23:48:45 +0900

    [level 1] Title: 최소직사각형, Time: 1.61 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        
+        for(int i = 0; i < sizes.length; i++){
+            if (sizes[i][0] > sizes[i][1]) {
+                max1 = Math.max(max1,sizes[i][0]);
+                max2 = Math.max(max2,sizes[i][1]);
+            } else {
+                max1 = Math.max(max1,sizes[i][1]);
+                max2 = Math.max(max2,sizes[i][0]);
+            }
+        }
+        
+        return max1 * max2;
+    }
+}
