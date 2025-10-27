e26a585b24f10037dbd5cffb2120ad7b48ff2d58
thdus <107402806+thdus@users.noreply.github.com>
2025-10-27 13:08:40 +0900

    [level 1] Title: 모의고사, Time: 0.64 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 88.6 MB, 시간: 0.79 ms
+메모리: 88.3 MB, 시간: 0.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 27일 12:37:21
+2025년 10월 27일 13:08:38
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,39 +1,38 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        // 수포자별 찍는 패턴
-        int[] pattern1 = {1, 2, 3, 4, 5};
-        int[] pattern2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] pattern3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-
-        // 각 수포자의 점수 저장
+        int[] answer = {};
+        
+        int[] one = { 1, 2, 3, 4, 5};
+        int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] three = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        
         int[] score = new int[3];
-
-        // 정답 비교
-        for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == pattern1[i % pattern1.length]) score[0]++;
-            if (answers[i] == pattern2[i % pattern2.length]) score[1]++;
-            if (answers[i] == pattern3[i % pattern3.length]) score[2]++;
-        }
-
-        // 가장 높은 점수 찾기
-        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
-
-        // 최고 점수를 받은 사람 목록 생성
-        List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            if (score[i] == maxScore) result.add(i + 1);
+        
+        for(int i=0; i<answers.length; i++){
+            if(answers[i]==one[i%one.length]) score[0]++;
+            if(answers[i]==two[i%two.length]) score[1]++;
+            if(answers[i]==three[i%three.length]) score[2]++;
         }
         
-        System.out.println(result);
-        // List → int[]
-        int cnt = 0;
-        int r[] = new int[result.size()];
-        for (int i : result) {
-		        r[cnt++] = i;
+        int max = Math.max(score[0], Math.max(score[1],score[2]));
+        
+        List<Integer> result = new ArrayList();
+        
+        for(int i=0; i<3; i++){
+            if(max==score[i]) result.add(i+1);
         }
+        
+        int[] r = new int[result.size()];
+        
+        int cnt=0;
+        
+        for(int i:result){
+            r[cnt++] = i;
+        }
+        
         return r;
     }
 }
-
