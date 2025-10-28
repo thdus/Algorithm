21cd3abc6eaf5a580c3f73dd1e1a864dd6d5d58a
thdus <107402806+thdus@users.noreply.github.com>
2025-10-28 14:26:37 +0900

    [level 2] Title: 소수 찾기, Time: 42.08 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,44 +1,44 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
-    // 전역 변수
-    static boolean[] visited; // 방문 여부
-    static Set<Integer> numberSet = new HashSet<>(); // 중복 제거용 집합
-
+    static boolean[] visited;
+    static Set<Integer> numberSet = new HashSet();
+    
+    
     public int solution(String numbers) {
-        visited = new boolean[numbers.length()];
-
-        // 모든 길이의 조합 생성
-        dfs("", numbers);
-
-        int count = 0;
-        for (int num : numberSet) {
-            if (isPrime(num)) count++;
-        }
-        return count;
+        
+       visited = new boolean[numbers.length()];
+        
+        int cnt=0;
+        
+        dfs("",numbers);
+        
+        for(int num : numberSet){
+            if(isPrime(num)){
+                cnt++;
+            }
+        }return cnt;
     }
-
-    // 깊이 우선 탐색으로 가능한 모든 조합 생성
-    private void dfs(String current, String numbers) {
-        if (!current.equals("")) {
+    
+    private void dfs(String current, String numbers){
+        if(!current.equals("")){
             numberSet.add(Integer.parseInt(current));
         }
-
-        for (int i = 0; i < numbers.length(); i++) {
-            if (!visited[i]) {
+        
+        for(int i=0; i<numbers.length(); i++){
+            if(!visited[i]){
                 visited[i] = true;
-                dfs(current + numbers.charAt(i), numbers);
+                dfs(current+numbers.charAt(i),numbers);
                 visited[i] = false;
             }
         }
     }
-
-    // 소수 판별
-    private boolean isPrime(int num) {
-        if (num < 2) return false;
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) return false;
-        }
-        return true;
+    
+    private boolean isPrime(int num){
+        if(num<2) return false;
+        for(int i=2; i<=Math.sqrt(num); i++){
+            if(num%i==0) return false;
+        } return true;
     }
 }
