8f762b542b696ad131a8ec62f27af38569caac65
thdus <107402806+thdus@users.noreply.github.com>
2025-10-30 00:55:29 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,19 +1,17 @@
-class Solution {
+import java.util.*;
+
+class Solution {    
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-        int x=1;
-        while(x<=Math.sqrt(yellow)){
-            if(yellow%x==0){
-                if((yellow/x+2)*(x+2)-yellow==brown){
-                    answer[0] = yellow/x+2;
-                    answer[1] = x+2;
-                    break;
-                }else{
-                    x++;
+        for(int i=1; i<=Math.sqrt(yellow); i++){
+            if(yellow%i==0){
+                int a = i;
+                int b = yellow/i;
+                if((a+2)*(b+2)-brown==yellow){
+                    answer[0] = b+2;
+                    answer[1] = a+2;
                 }
-            }else{
-                x++;
             }
         }
         
