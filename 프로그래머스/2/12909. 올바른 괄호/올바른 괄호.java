d9f0584fe8c75e978258a68f6a006d4fa2095013
thdus <107402806+thdus@users.noreply.github.com>
2025-10-30 20:26:28 +0900

    [level 2] Title: 올바른 괄호, Time: 7.00 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,23 +2,27 @@ class Solution {
     boolean solution(String s) {
         boolean answer = true;
 
-        int cnt=0;
+        if(s.charAt(0)==')') return false;
+        if(s.charAt(s.length()-1)=='(') return false;
+        
+        int cnt = 0;
+        
         for(int i=0; i<s.length(); i++){
-            if(s.charAt(i)=='('){
+            if(s.charAt(i)=='(') {
                 cnt++;
-            }else{
-                if(cnt<=0){
-                    return false;
-                }else{
-                    cnt--;
-                }
+            } else{
+                cnt--;
             }
+            
+            if(cnt<0) return false;
         }
-        if(cnt!=0){
-            return false;
-        }else{
+        
+        if(cnt==0){
             return true;
+        } else{
+            return false;
         }
 
+       
     }
 }
