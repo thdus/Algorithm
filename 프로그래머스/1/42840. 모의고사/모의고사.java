5f038bd4c35479cb62718648e9756ff4b422e517
thdus <107402806+thdus@users.noreply.github.com>
2025-10-30 14:02:05 +0900

    [level 1] Title: 모의고사, Time: 1.03 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,13 +1,10 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] answer = {};
-        
-        int[] one = { 1, 2, 3, 4, 5};
+        int[] one = {1, 2, 3, 4, 5};
         int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] three = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
         int[] score = new int[3];
         
@@ -19,20 +16,19 @@ class Solution {
         
         int max = Math.max(score[0], Math.max(score[1],score[2]));
         
-        List<Integer> result = new ArrayList();
+        List<Integer> return1 = new ArrayList();
         
         for(int i=0; i<3; i++){
-            if(max==score[i]) result.add(i+1);
+            if(max==score[i]){
+                return1.add(i+1);
+            }
         }
-        
-        int[] r = new int[result.size()];
         
         int cnt=0;
-        
-        for(int i:result){
-            r[cnt++] = i;
+        int[] ans = new int[return1.size()];
+        for(int n:return1){
+            ans[cnt++] = n;
         }
-        
-        return r;
+       return ans;
     }
 }
