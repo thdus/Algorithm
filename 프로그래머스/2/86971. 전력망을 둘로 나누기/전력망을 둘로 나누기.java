409d6fdf94710c4d70e2a6628572722dc464f7b3
thdus <107402806+thdus@users.noreply.github.com>
2025-10-30 22:43:56 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 12.17 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,8 +1,13 @@
 import java.util.*;
 
 class Solution {
+    static List<List<Integer>> graph;
+    static boolean[] visited;
+    static int cnt;
+    
     public int solution(int n, int[][] wires) {
-        List<List<Integer>> graph = new ArrayList();
+        
+        graph = new ArrayList();
         for(int i=0; i<=n; i++){
             graph.add(new ArrayList());
         }
@@ -10,47 +15,51 @@ class Solution {
         for(int[] wire : wires){
             int a = wire[0];
             int b = wire[1];
+            
             graph.get(a).add(b);
             graph.get(b).add(a);
         }
-        
         int ans = Integer.MAX_VALUE;
-        
         for(int[] cut : wires){
             int a = cut[0];
             int b = cut[1];
-            
-            int cnt = bfs(graph,n,a,b);
-            
-            int diff= Math.abs((n-cnt)-cnt);
-            ans = Math.min(ans, diff);
+         
+            int cnt = bfs(graph, n, a, b);
+            System.out.println(cnt);
+            int diff = Math.abs((n-cnt)-cnt);
+            ans = Math.min(diff, ans);
         }
         return ans;
         
         
+        
+      
     }
     
-    private int bfs(List<List<Integer>> graph, int n, int a, int b){
-        boolean[] visited = new boolean[n+1];
+    private int bfs(List<List<Integer>> graph, int n, int start, int end ){
+        visited = new boolean[n+1];
         Queue<Integer> q = new LinkedList();
-        q.add(a);
-        visited[a] = true;
-       
-        int cnt = 1;
+        visited[start] = true;
+        q.add(start);
+        int cnt=1;
         
         while(!q.isEmpty()){
             int cur = q.poll();
             
-            for(int next : graph.get(cur)){
-                if((cur==a&&next==b)||(cur==b&&next==a)) continue;
+            
+                for(int next: graph.get(cur)){
+                if((cur==start&&next==end)||(cur==end&&next==start)) continue;
                 
                 if(!visited[next]){
-                    visited[next] = true;
-                    q.add(next);
-                    cnt++;
-                }
+                visited[next] = true;
+                q.add(next);
+                cnt++;
             }
-        }
-        return cnt;
+            }
+            
+            
+            
+        }return cnt;
+        
     }
 }
