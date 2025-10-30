8fb40ec166f54a625f8f6e460ef9bda9a501cb99
thdus <107402806+thdus@users.noreply.github.com>
2025-10-30 21:43:55 +0900

    [level 2] Title: 모음 사전, Time: 12.63 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,25 +1,27 @@
 import java.util.*;
 
 class Solution {
-    static List<String> dict = new ArrayList<>();
-    static String[] vowels = {"A", "E", "I", "O", "U"};
-
+    static List<String> dict = new ArrayList();
+    static String[] vowel = {"A","E","I","O","U"};
+    
     public int solution(String word) {
-        // 1️⃣ 모든 단어를 사전순으로 미리 생성
-        dfs("", 0);
-
-        // 2️⃣ 단어 리스트에서 해당 단어의 위치 찾기 (indexOf)
-        return dict.indexOf(word) + 1;
+        
+        dfs("",0);
+        
+        return dict.indexOf(word)+1;
     }
-
-    // DFS로 길이 5 이하의 모든 조합 생성
-    private void dfs(String str, int depth) {
-        if (depth > 5) return;
-
-        if (!str.equals("")) dict.add(str); // 빈 문자열 제외 후 추가
-
-        for (String v : vowels) {
-            dfs(str + v, depth + 1);
+    
+    private void dfs(String s, int depth){
+        if(depth>5){
+            return;
         }
+        
+        if(!s.equals("")) dict.add(s);
+        
+        for(String v: vowel){
+            dfs(s+v, depth+1);
+        }
+        
+        
     }
 }
