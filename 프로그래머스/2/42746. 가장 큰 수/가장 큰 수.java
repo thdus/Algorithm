8acc763bd7e4899e2797e757b62fabd7866d3a3b
thdus <107402806+thdus@users.noreply.github.com>
2025-10-30 23:23:42 +0900

    [level 2] Title: 가장 큰 수, Time: 281.59 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -3,22 +3,22 @@ import java.io.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] arr = new String[numbers.length];
-        for(int i =0; i<numbers.length; i++){
-            arr[i] = String.valueOf(numbers[i]);
+        String[] s = new String[numbers.length];
+        
+        for(int i=0; i<numbers.length; i++){
+            s[i] = numbers[i]+"";
         }
         
-        Arrays.sort(arr,(a,b)->(b+a).compareTo(a+b));
+        Arrays.sort(s, (a,b) -> (b+a).compareTo(a+b));
         
-        if(arr[0].equals("0")) return "0";
+        if (s[0].equals("0")) return "0";
         
         StringBuilder sb = new StringBuilder();
-        for(String s : arr){
-            sb.append(s);
+        for(String n:s){
+           
+            sb.append(n);
         }
         
         return sb+"";
-        
-        
     }
 }
