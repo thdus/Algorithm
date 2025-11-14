148598f5ede9ec42c7516ecfe216fa78a921ae38
thdus <107402806+thdus@users.noreply.github.com>
2025-11-14 17:15:05 +0900

    [Silver V] Title: 영화감독 숌, Time: 288 ms, Memory: 113864 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -3,18 +3,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int cnt = 0;
 		
-		for (int i = 0; i < Integer.MAX_VALUE; i++) {
-			if ((i + "").contains("666"))
+		int n = Integer.parseInt(br.readLine());
+		int cnt =0;
+		for(int i=0; i<Integer.MAX_VALUE; i++) {
+			
+			if((i+"").contains("666")) {
 				cnt++;
-			if (cnt == n) {
-				System.out.println(i);
-				return;
+				if(cnt==n) {
+					System.out.println(i);
+					break;
+				}
 			}
 		}
+		
+		
 	}
 }
