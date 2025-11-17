3dd87d9b8fa39675e04205574e8ed8d72a89fec3
thdus <107402806+thdus@users.noreply.github.com>
2025-11-17 19:54:23 +0900

    [Silver V] Title: 단어 정렬, Time: 372 ms, Memory: 33168 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,34 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int N = in.nextInt();
-		String[] arr = new String[N];
-		in.nextLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		for (int i = 0; i < N; i++) {
-			arr[i] = in.nextLine();
-		}
-		
-		Arrays.sort(arr, new Comparator<String>() {
-			public int compare(String s1, String s2) {
-				if (s1.length() == s2.length()) {
-					return s1.compareTo(s2);
-				} else {
-					return s1.length() - s2.length();
-				}
-			}
-		});
+        int N = Integer.parseInt(br.readLine());
+        String[] words = new String[N];
 
-		System.out.println(arr[0]);
-		
-		for (int i = 1; i < N; i++) {
-			if (!arr[i].equals(arr[i - 1])) {
-				System.out.println(arr[i]);
-			}
-		}
-	}
+        for (int i = 0; i < N; i++) {
+            words[i] = br.readLine();
+        }
+
+        // 정렬: 1) 길이 오름차순  2) 길이 같으면 사전순
+        Arrays.sort(words, (o1, o2) -> {
+            if (o1.length() == o2.length()) {
+                return o1.compareTo(o2); // 사전순
+            } else {
+                return o1.length() - o2.length(); // 길이순
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        String prev = "";
+
+        for (int i = 0; i < N; i++) {
+            // 바로 이전 단어와 같으면(중복) 건너뛰기
+            if (!words[i].equals(prev)) {
+                sb.append(words[i]).append('\n');
+                prev = words[i];
+            }
+        }
+
+        System.out.print(sb);
+    }
 }
