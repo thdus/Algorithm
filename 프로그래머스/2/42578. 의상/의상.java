1f99e26c61bc0016a1445f5b7f24c8068bf10927
thdus <107402806+thdus@users.noreply.github.com>
2026-03-01 16:58:43 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,24 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-      
-        Map<String, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < clothes.length; i++) {
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<clothes.length; i++){
             String type = clothes[i][1];
-
-            if (map.containsKey(type)) {
-                map.put(type, map.get(type) + 1);
-            } else {
-                map.put(type, 1);
+            
+            if(map.containsKey(type)){
+                map.put(type,map.get(type)+1);
+            }else{
+                map.put(type,1);
             }
         }
-
-        int answer = 1;
-        for (int count : map.values()) {
-            answer *= (count + 1); 
-        }
         
-        return answer - 1;
+        int answer = 1;
+        for(int count:map.values()){
+            answer*=(count+1);
+        }
+        return answer-1;
     }
 }
