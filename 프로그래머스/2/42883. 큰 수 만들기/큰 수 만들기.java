d4ab010ea067f625f4a6e1ec39c041322ebcca8d
thdus <107402806+thdus@users.noreply.github.com>
2026-03-01 22:31:11 +0900

    Refactor solution method for clarity and efficiency

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,24 +1,23 @@
 class Solution {
     public String solution(String number, int k) {
        
+        StringBuilder sb = new StringBuilder();
         
-         StringBuilder sb = new StringBuilder();
-    int remove = k;
-    
-    for (int i = 0; i < number.length(); i++) {
-        char current = number.charAt(i);
+        int remove = k;
         
-        // 앞에 작은 숫자 제거
-        while (remove > 0 && sb.length() > 0 
-               && sb.charAt(sb.length() - 1) < current) {
-            sb.deleteCharAt(sb.length() - 1);
-            remove--;
+        for(int i=0; i<number.length(); i++){
+            char c = number.charAt(i);
+            
+            while(remove>0&&sb.length()>0&&
+                 sb.charAt(sb.length()-1)<c){
+                sb.deleteCharAt(sb.length()-1);
+                remove--;
+            }
+            
+            sb.append(c);
         }
         
-        sb.append(current);
-    }
-    
-    // 아직 제거할 수가 남았으면 뒤에서 제거
-    return sb.substring(0, sb.length() - remove);
+        return sb.substring(0, sb.length()-remove);
     }
 }
+
