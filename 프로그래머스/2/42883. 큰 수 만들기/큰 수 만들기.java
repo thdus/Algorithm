afa3aeab7d3829ef7f11c1224696398464f263ff
thdus <107402806+thdus@users.noreply.github.com>
2026-03-01 22:21:48 +0900

    [level 2] Title: 큰 수 만들기, Time: 25.46 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String number, int k) {
+       
+        
+         StringBuilder sb = new StringBuilder();
+    int remove = k;
+    
+    for (int i = 0; i < number.length(); i++) {
+        char current = number.charAt(i);
+        
+        // 앞에 작은 숫자 제거
+        while (remove > 0 && sb.length() > 0 
+               && sb.charAt(sb.length() - 1) < current) {
+            sb.deleteCharAt(sb.length() - 1);
+            remove--;
+        }
+        
+        sb.append(current);
+    }
+    
+    // 아직 제거할 수가 남았으면 뒤에서 제거
+    return sb.substring(0, sb.length() - remove);
+    }
+}
