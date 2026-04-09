06738f01bb8ef6b23a2029f726802c318ef169d9
thdus <107402806+thdus@users.noreply.github.com>
2026-04-10 00:49:47 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,6 +1,6 @@
 with recursive generations as(
     select id, parent_id, 1 as generation
-    from ECOLI_DATA 
+    from ECOLI_DATA
     where parent_id is null
     
     union all
@@ -8,7 +8,8 @@ with recursive generations as(
     select e.id, e.parent_id, g.generation+1
     from ECOLI_DATA as e
     join generations as g on e.parent_id=g.id
-)select count(*) as count , g.generation as generation
+)
+select count(*) as count, g.generation as generation
 from generations as g
 where not exists(
     select 1
