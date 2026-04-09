aa5bb812f1101f624a1d0d7f88f1f445f9df2bfd
thdus <107402806+thdus@users.noreply.github.com>
2026-04-10 00:08:07 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,34 +1,22 @@
 -- 코드를 작성해주세요
-
-WITH RECURSIVE generations AS (
-    -- 1세대: 부모가 없는 개체들
-    SELECT
-        ID,
-        PARENT_ID,
-        1 AS GENERATION
-    FROM ECOLI_DATA
-    WHERE PARENT_ID IS NULL
-
-    UNION ALL
-
-    -- 그 다음 세대: 이전 세대의 자식들
-    SELECT
-        e.ID,
-        e.PARENT_ID,
-        g.GENERATION + 1
-    FROM ECOLI_DATA e
-    JOIN generations g
-      ON e.PARENT_ID = g.ID
+with recursive generations as(
+    select id, parent_id, 1 as generation
+    from ECOLI_DATA
+    where parent_id is null
+    
+    union all
+    
+    select e.id, e.parent_id, g.generation+1
+    from ECOLI_DATA e
+    join generations g on e.parent_id = g.id
 )
-SELECT
-    COUNT(*) AS COUNT,
-    g.GENERATION AS GENERATION
-FROM generations g
-WHERE NOT EXISTS (
-    -- 자식이 없는 개체만 선택
-    SELECT 1
-    FROM ECOLI_DATA child
-    WHERE child.PARENT_ID = g.ID
+
+select count(*) as count, g.generation as generation
+from generations g
+where not exists(
+    select 1
+    from ECOLI_DATA child
+    where child.parent_id = g.id
 )
-GROUP BY g.GENERATION
-ORDER BY g.GENERATION ASC;
+group by g.generation
+order by g.generation;
