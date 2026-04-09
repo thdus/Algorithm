793028e52b38b3575c5668970494d55e63fca067
thdus <107402806+thdus@users.noreply.github.com>
2026-04-10 00:45:14 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,4 +1,3 @@
--- 코드를 작성해주세요
 with recursive generations as(
     select id, parent_id, 1 as generation
     from ECOLI_DATA 
@@ -7,15 +6,14 @@ with recursive generations as(
     union all
     
     select e.id, e.parent_id, g.generation+1
-    from ECOLI_DATA e
-    join generations g on e.parent_id=g.id
-)
-select count(*) as count, g.generation as generation
-from generations g
+    from ECOLI_DATA as e
+    join generations as g on e.parent_id=g.id
+)select count(*) as count , g.generation as generation
+from generations as g
 where not exists(
     select 1
     from ECOLI_DATA child
-    where child.parent_id = g.id
+    where child.parent_id=g.id
 )
-group by g.generation
-order by g.generation;
+group by generation
+order by generation;
