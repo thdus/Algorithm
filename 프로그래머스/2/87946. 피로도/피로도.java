77f1324a3b6195a62c98a013bc2ea6eb170d4c2d
thdus <107402806+thdus@users.noreply.github.com>
2026-04-10 18:55:12 +0900

    [level 2] Title: 피로도, Time: 3.92 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,22 +2,25 @@ import java.util.*;
 
 class Solution {
     static boolean[] visited;
-    static int max = 0;
-
+    static int max;
+    
     public int solution(int k, int[][] dungeons) {
         visited = new boolean[dungeons.length];
-        dfs(dungeons, k, 0);
+        
+        max = 0;
+       
+        dfs(dungeons,k,0);
         return max;
     }
-
-    public static void dfs(int[][] dungeons, int p, int cnt) {
+    
+    public static void dfs(int[][] dungeons, int p, int cnt){
         max = Math.max(max, cnt);
-
-        for (int i = 0; i < dungeons.length; i++) {
-            if (!visited[i] && p >= dungeons[i][0]) {
-                visited[i] = true;
-                dfs(dungeons, p - dungeons[i][1], cnt + 1);
-                visited[i] = false;
+        
+        for(int i=0; i<dungeons.length; i++){
+            if(!visited[i]&&p>=dungeons[i][0]){
+                visited[i]=true;
+                dfs(dungeons, p-dungeons[i][1],cnt+1);
+                visited[i]=false;
             }
         }
     }
