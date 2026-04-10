d7c1b6ce47b870206b1152157a8b3c26c3b5bc17
thdus <107402806+thdus@users.noreply.github.com>
2026-04-10 18:44:42 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,25 +1,23 @@
+import java.util.*;
+
 class Solution {
     static int x;
     static int y;
+    
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        
         int sum = brown+yellow;
+      
         for(int i=1; i<=yellow; i++){
-            if(yellow%i==0){
+            if(yellow%i==0&&(i+2)*(yellow/i+2)==sum){
                 y=i;
-                x =yellow/i;
-                System.out.println(x);
-                System.out.println(y);
-                
-                if((x+2)*(i+2)==sum) 
+                x = yellow/i;
                 break;
             }
         }
         answer[0]=x+2;
         answer[1]=y+2;
         
-
         return answer;
     }
 }
