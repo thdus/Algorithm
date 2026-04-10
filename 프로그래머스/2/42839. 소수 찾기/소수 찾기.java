5a28eacdc0add2846d034c428deac2eb62d85591
thdus <107402806+thdus@users.noreply.github.com>
2026-04-10 18:26:04 +0900

    [level 2] Title: 소수 찾기, Time: 25.60 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,45 +2,39 @@ import java.util.*;
 
 class Solution {
     static boolean[] visited;
-    static Set<Integer> numberSet;
+    static Set<Integer> numberSet = new HashSet<>();
     
     public int solution(String numbers) {
-        visited = new boolean[numbers.length()];
-        numberSet = new HashSet();
-        dfs("", numbers);
-        
-        int cnt =0;
-        for(int n:numberSet){
-           
-            if(isPrime(n)){
-                 System.out.println(n);
+        visited= new boolean[numbers.length()];
+        int cnt = 0;
+        dfs("",numbers);
+        for(int num:numberSet){
+            if(isPrime(num)){
                 cnt++;
             }
         }
         
-          return cnt;
+        return cnt;
     }
-
     
-
-    
-    private void dfs(String s, String numbers){
-        if(!s.equals("")) numberSet.add(Integer.parseInt(s));
+    public static void dfs(String current, String numbers){
+        if(!current.equals("")){
+            numberSet.add(Integer.parseInt(current));
+        }
         
         for(int i=0; i<numbers.length(); i++){
             if(!visited[i]){
-                visited[i] = true;
-                dfs(s+numbers.charAt(i), numbers);
-                visited[i] = false;
+                visited[i]=true;
+                dfs(current+numbers.charAt(i),numbers);
+                visited[i]=false;
             }
         }
     }
-
-    private boolean isPrime(int num){
+    
+    public static boolean isPrime(int num){
         if(num<2) return false;
         for(int i=2; i<=Math.sqrt(num); i++){
             if(num%i==0) return false;
-        }
-    return true;
+        }return true;
     }
 }
