e6974d95142582c3a45936a8a506c81c4249aabd
thdus <107402806+thdus@users.noreply.github.com>
2026-04-30 14:32:01 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,11 +1,11 @@
--- 코드를 작성해주세요
-select case when a.SKILL_CODE&b.fe>0 and a.SKILL_CODE&b.py>0 then 'A'
-            when a.SKILL_CODE&b.c>0 then 'B'
-            else 'C' end as GRADE, a.id as ID, a.email as EMAIL
-from DEVELOPERS a join (select 
-                        sum(case when category='Front End' then code end) as fe,
-                        max(case when name='Python' then code end) as py,
-                        max(case when name='C#' then code end) as c
-                       from SKILLCODES) as b
-where b.fe&a.SKILL_CODE>0 or a.SKILL_CODE&b.c>0
-order by grade, id;
+select case when a.SKILL_CODE&b.fe>0 and a.SKILL_CODE&b.py>0 then "A"
+            when a.SKILL_CODE&b.c>0 then "B"
+            else "C" end as GRADE, a.id as ID, a.email as EMAIL
+
+from DEVELOPERS  a join (select
+    sum(case when category='Front End' then code end) as fe,
+    max(case when name='Python' then code end) as py,
+    max(case when name='C#' then code end) as c
+    from SKILLCODES) b
+where a.SKILL_CODE&b.fe>0 or a.SKILL_CODE&b.c
+order by grade, a.id;
